eeaa1961da342e98ef4ffc8c087d45227ac8c5bc
CodingManoj <CodingManoj@gmail.com>
2025-06-08 21:53:27 -0400

    Roboshop v1: Provisioning VPC via Terraform

--- subnets.tf
+++ subnets.tf
@@ -1,23 +1,26 @@
 resource "aws_subnet" "web" {
-  count      = length(var.web_subnet_cidr_block)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.web_subnet_cidr_block[count.index]
+  count             = length(var.web_subnet_cidr_block)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.web_subnet_cidr_block[count.index]
+  availability_zone = var.azs[count.index]
 
   tags = local.web_subnet_tags
 }
 
 resource "aws_subnet" "app" {
-  count      = length(var.app_subnet_cidr_block)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.app_subnet_cidr_block[count.index]
+  count             = length(var.app_subnet_cidr_block)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.app_subnet_cidr_block[count.index]
+  availability_zone = var.azs[count.index]
 
   tags = local.app_subnet_tags
 }
 
 resource "aws_subnet" "db" {
-  count      = length(var.db_subnet_cidr_block)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.db_subnet_cidr_block[count.index]
+  count             = length(var.db_subnet_cidr_block)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.db_subnet_cidr_block[count.index]
+  availability_zone = var.azs[count.index]
 
   tags = local.db_subnet_tags
 }
