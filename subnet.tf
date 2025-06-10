b91c323a12e2976df0e17a8d040c278c968ac26a
CodingManoj <CodingManoj@gmail.com>
2025-06-09 21:09:10 -0400

    Roboshop v1: Provisioning VPC via Terraform

--- subnet.tf
+++ subnet.tf
@@ -1,26 +1,26 @@
 resource "aws_subnet" "web" {
-  count             = length(var.web_subnet_cidr_block)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.web_subnet_cidr_block[count.index]
-  availability_zone = var.az[count.index]
+  count      = length(var.web_subnet_cidr_block)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.web_subnet_cidr_block[count.index]
+  # availability_zone = var.az[count.index]
   # The tags for the web subnet
   tags = local.web_subnet_tags
 }
 
 resource "aws_subnet" "app" {
-  count             = length(var.app_subnet_cidr_block)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.app_subnet_cidr_block[count.index]
-  availability_zone = var.az[count.index]
+  count      = length(var.app_subnet_cidr_block)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.app_subnet_cidr_block[count.index]
+  # availability_zone = var.az[count.index]
 
   tags = local.app_subnet_tags
 }
 
 resource "aws_subnet" "db" {
-  count             = length(var.db_subnet_cidr_block)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.db_subnet_cidr_block[count.index]
-  availability_zone = var.az[count.index]
+  count      = length(var.db_subnet_cidr_block)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.db_subnet_cidr_block[count.index]
+  # availability_zone = var.az[count.index]
 
   tags = local.db_subnet_tags
 }
