9cd770ee5ed9d23ee058e1ab4c432c5c475c0a30
CodingManoj <CodingManoj@gmail.com>
2025-06-11 20:54:42 -0400

    Roboshop v1: Provisioning VPC via Terraform

--- locals.tf
+++ locals.tf
@@ -1,8 +1,7 @@
 locals {
   tags            = merge(var.tags, { module_name = "tf-module-vpc" })
   name_tag_prefix = var.env
-  web_subnet_tags = merge(local.tags, { Name = "${local.name_tag_prefix}-web-subnet-${local.zonename}" })
-  app_subnet_tags = merge(local.tags, { Name = "${local.name_tag_prefix}-app-subnet-${local.zonename}" })
-  db_subnet_tags  = merge(local.tags, { Name = "${local.name_tag_prefix}-db-subnet-${local.zonename}" })
-  zonename        = var.az[count.index]
+  web_subnet_tags = merge(local.tags, { Name = "${local.name_tag_prefix}-web-subnet" })
+  app_subnet_tags = merge(local.tags, { Name = "${local.name_tag_prefix}-app-subnet" })
+  db_subnet_tags  = merge(local.tags, { Name = "${local.name_tag_prefix}-db-subnet" })
 }
