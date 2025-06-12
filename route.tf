07b4ba13923ff84fd703b78c6bf1d108f8467d24
CodingManoj <CodingManoj@gmail.com>
2025-06-11 20:57:09 -0400

    Roboshop v1: Provisioning VPC via Terraform

--- route.tf
+++ route.tf
@@ -22,3 +22,25 @@ resource "aws_route_table" "web" {
     Name = "${local.web_subnet_tags}-route-table"
   }
 }
+
+resource "aws_route_table" "app" {
+  vpc_id = aws_vpc.main.id
+
+  # route {
+  #   cidr_block = "0.0.0.0/0"
+  #   gateway_id = aws_internet_gateway.main.id
+  # }
+
+  tags = {
+    Name = "${local.app_subnet_tags}-route-table"
+  }
+}
+
+resource "aws_route_table" "db" {
+  vpc_id = aws_vpc.main.id
+
+
+  tags = {
+    Name = "${local.db_subnet_tags}-route-table"
+  }
+}
