20cf2b79b1db63df55440ee314967a86b13f556c
CodingManoj <CodingManoj@gmail.com>
2025-06-11 21:33:08 -0400

    Roboshop v1: Provisioning VPC via Terraform

---
+++ gateways.tf
@@ -0,0 +1,28 @@
+# Internet Gateway for the VPC
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "roboshop-${local.name_tag_prefix}-igw"
+  }
+}
+
+# Reserves an Elastic IP for the NAT Gateway
+resource "aws_eip" "main" {
+  domain = "vpc"
+  tags = {
+    Name = "roboshop-${local.nat_tags}-eip"
+  }
+}
+
+# Provisions a NAT Gateway in the public subnet
+resource "aws_nat_gateway" "main" {
+  depends_on = [aws_eip.main]
+
+  allocation_id = aws_eip.main.id
+  subnet_id     = aws_subnet.web.*.id[0]
+
+  tags = {
+    Name = "roboshop-${local.nat_tags}"
+  }
+}
--- locals.tf
+++ locals.tf
@@ -4,4 +4,6 @@ locals {
   web_subnet_tags = merge(local.tags, { Name = "${local.name_tag_prefix}-web-subnet" })
   app_subnet_tags = merge(local.tags, { Name = "${local.name_tag_prefix}-app-subnet" })
   db_subnet_tags  = merge(local.tags, { Name = "${local.name_tag_prefix}-db-subnet" })
+  igw_tags        = merge(local.tags, { Name = "${local.name_tag_prefix}-igw" })
+  nat_tags        = merge(local.tags, { Name = "${local.name_tag_prefix}-nat-gw" })
 }
